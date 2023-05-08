7542e6ebeb92f9ce2e12647d8c321a1a01294ca1
Leonardo Morais Franca <mfrancaleonardo@gmail.com>
2023-05-08 14:10:43 -0300

    comentarios

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <!-- O router view tem que estar no wrap do vue fragment para evitar o erro que
+    começou a dar a partir da versão 1.5.2 do vue fragment, fazendo com que os cards
+    sejam movidos sem erros! -->
     <fragment>
       <router-view></router-view>
     </fragment>
