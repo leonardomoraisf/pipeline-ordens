84d89d4367c01deeb7d35d64379fc33d7e71f121
Leonardo Morais Franca <mfrancaleonardo@gmail.com>
2023-05-11 13:48:40 -0300

    correção

--- src/components/BoardKanban.vue
+++ src/components/BoardKanban.vue
@@ -370,7 +370,7 @@ export default {
         card.indexInative = e.moved.newIndex;
 
         if (card.timer === 60) {
-          this.cardsToInativeList.array.forEach((card, index) => {
+          this.cardsToInativeList.forEach((card, index) => {
             if(card === undefined) {
                 this.cardsToInativeList.slice(index,1);
             }
